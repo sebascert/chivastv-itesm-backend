4055af3f7fcd8fdb780d826fb4ff7c2334b858c8
Sebastian Certuche <sebascertuche@gmail.com>
2025-06-12 15:36:13 -0600

    Fix resource dependency arg names to match path args

--- src/dependencies/resource.py
+++ src/dependencies/resource.py
@@ -1,16 +1,16 @@
 from uuid import UUID
 
 
-async def get_category_by_name(name: str) -> UUID:
+async def get_category_by_name(category: str) -> UUID:
     """get and validate a video category"""
     raise NotImplementedError()
 
 
-async def get_video_by_id(id: str) -> UUID:
+async def get_video_by_id(video_id: str) -> UUID:
     """get and validate a video by it's id"""
     raise NotImplementedError()
 
 
-async def get_comment_by_id(id: str) -> UUID:
+async def get_comment_by_id(comment_id: str) -> UUID:
     """get and validate a comment by it's id"""
     raise NotImplementedError()
